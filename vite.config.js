3f67890644ca2cabda7e850664ffab36060a005f
nizw0 <nizw0p@gmail.com>
2022-07-09 05:06:14 +0800

    chore: init

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  define: {
+    global: {}
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      './runtimeConfig': './runtimeConfig.browser'
+    }
+  }
+})
