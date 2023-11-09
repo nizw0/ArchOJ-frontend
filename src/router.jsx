29efce9618bcb442683a9a6cbf62608b77e90af8
nizw0 <nizw0p@gmail.com>
2023-11-09 19:51:05 +0800

    u

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
+import Sidebar from './components/sidebar'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <></>,
-    children: [
-      {
-        path: '',
-        element: <></>,
-      },
-    ],
+    element: (
+      <>
+        <Sidebar>
+          <div>123</div>
+        </Sidebar>
+      </>
+    ),
+    // children: [
+    //   {
+    //     path: '/',
+    //     element: <></>,
+    //   },
+    // ],
   },
 ])
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter var',
+          ...require('tailwindcss/defaultTheme').fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
