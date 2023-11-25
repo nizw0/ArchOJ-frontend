6699408b5b67187181201d5284680519d8ec46b3
nizw0 <nizw0p@gmail.com>
2023-11-26 07:24:28 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
       './runtimeConfig': './runtimeConfig.browser',
     },
   },
