2bbb22ddf48f9ebd0917e9e55344bfa0e7ba8470
nizw0 <nizw0p@gmail.com>
2023-11-26 06:34:37 +0800

    l

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  define: {
-    global: {},
-  },
   plugins: [react()],
   resolve: {
     alias: {
