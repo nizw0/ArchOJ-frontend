0834b92553a41e72fab6afee64f6c79dd9c94783
nizw0 <nizw0p@gmail.com>
2023-11-26 18:42:31 +0800

    test

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,16 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { Amplify } from 'aws-amplify'
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router'
 import { RecoilRoot } from 'recoil'
+import amplifyConfig from './aws-exports.js'
 import Loading from './components/loading'
 import { queryClient } from './query'
 import { router } from './router'
+
+Amplify.configure(amplifyConfig)
+console.log(amplifyConfig.Auth.Cognito)
 
 export default function App() {
   return (
