4b33c7903a099d3158eb13d0e2e1ba3319e5d318
nizw0 <nizw0p@gmail.com>
2023-11-26 08:35:14 +0800

    x

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import react from '@vitejs/plugin-react'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+import { rollupNodePolyFill } from 'rollup-plugin-node-polyfills'
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: {},
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
