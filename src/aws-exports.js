94d19d7dacef3fed0d77d11d2e3a722eabb4d2fc
nizw0 <nizw0p@gmail.com>
2023-11-26 17:54:23 +0800

    x

--- src/aws-exports.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('aws-amplify').ResourcesConfig} */
-export default {
-  Auth: {
-    Cognito: {
-      userPoolId: process.env.USER_POOL_ID,
-      userPoolClientId: process.env.USER_POOL_CLIENT_ID,
-    },
-  },
-}
