03b2ad130e64ff26a9fd0fd7a95239c54b0097b8
nizw0 <nizw0p@gmail.com>
2023-11-26 18:26:56 +0800

    x

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
-import { Amplify } from 'aws-amplify'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import amplifyConfig from '../aws-exports.js'
 import App from './app.jsx'
 import './index.css'
-
-Amplify.configure(amplifyConfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
